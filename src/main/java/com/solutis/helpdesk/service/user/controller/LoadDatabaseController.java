759f9cba663db22cc204d8fb3289d65e82e31101
caioCarmoAtSolutis <caio.carmo@solutis.com.br>
2026-09-19 16:34:22 -0300

    fix: Do fix typo in load database controller endpoint path

--- src/main/java/com/solutis/helpdesk/service/user/controller/LoadDatabaseController.java
+++ src/main/java/com/solutis/helpdesk/service/user/controller/LoadDatabaseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/load=data")
+@RequestMapping("/load-data")
 public class LoadDatabaseController {
     @Autowired
     private LoadDatabaseService loadDatabaseService;
