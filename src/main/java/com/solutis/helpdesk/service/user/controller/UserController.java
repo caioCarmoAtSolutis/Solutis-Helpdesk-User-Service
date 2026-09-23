ca9dc71faf05b51f0d10238d47f0c99f0972dda4
caioCarmoAtSolutis <caio.carmo@solutis.com.br>
2026-09-23 13:05:05 -0300

    feat: Do disable cors in user controller

--- src/main/java/com/solutis/helpdesk/service/user/controller/UserController.java
+++ src/main/java/com/solutis/helpdesk/service/user/controller/UserController.java
@@ -26,6 +26,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.util.UUID;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
